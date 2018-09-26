7685e69b948898becbad62a380285cd21038731e
andy.chadha2 <andy.chadha2@gmail.com>
2018-09-26 06:19:16 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -15,6 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:grace_hopper]
 
 
 end
@@ -37,6 +39,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -52,9 +56,11 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => "C"
         }
      }
+programmer_hash[:dennis_ritchie][:languages].to_s     
+
 end
 
 def adding_matz
@@ -79,7 +85,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[:yukihiro_matsumoto]
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
     
 end
 
@@ -101,9 +108,11 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+programmer_hash[:alan_kay][:known_for] = alans_new_info.to_s
      
      
 end
+
 
 def adding_to_dennis
 	programmer_hash = 
